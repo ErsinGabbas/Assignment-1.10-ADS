088ae85004023a86f0b3468e5145b85262e25687
ErsinGabbas <ersingabbas1315@gmail.com>
2023-04-04 21:45:41 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    //Function
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+    //Function work check
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int gcd = gcd(a, b);
+        System.out.println(gcd);
+    }
+}
